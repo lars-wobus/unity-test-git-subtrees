952047eeac8feeda1b6eeb1aad42924550cf01c5
Lars Wobus <lars.wobus@web.de>
2018-08-30 07:17:49 +0200

    Describing 2 ways to push changes to original repo

--- scripting/add-subtree.bat
+++ scripting/add-subtree.bat
@@ -12,7 +12,10 @@ git subtree add --prefix %LocalDirectory% %RepositoryName% master --squash
 git fetch %RepositoryName% master
 git subtree pull --prefix %LocalDirectory% %RepositoryName% master --squash
 
-:: When original repository should be updated. Fork this repository, push changes to the new repo and create a pull request to inform author of the original repository.
+:: When changes in a subtree should be published in its original remote repository, there are two way to accomplish that.
+:: Method 1: When you are the author of the original repository, you can simply execute the next command
+subtree push --prefix= %LocalDirectory% %RepositoryName% master
+:: Method 2: When you are not the author of the original repository, you have to fork this project , push changes to the new repository and create pull requests to inform the original author about your changes.
 git remote add %ForkedRepositoryName% %ForkedRepositoryUrl%
 git subtree push --prefix=%LocalDirectory% %ForkedRepositoryName% master
 
